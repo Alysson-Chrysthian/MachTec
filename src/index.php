03e99091891e8267a4766d450db41dec4d929c4f
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-05-08 20:56:34 -0300

    project foundation done

--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Alyssoncpc\Machtec\Domain\Router;
-
-require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/web.php');
-
--- src/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Alyssoncpc\Machtec\Domain\Methods;
-use Alyssoncpc\Machtec\Domain\Router;
-

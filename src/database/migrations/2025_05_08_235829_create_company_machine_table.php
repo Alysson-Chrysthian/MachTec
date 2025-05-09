739d9f88825122048099276ac7350bab8a7acef8
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-05-08 21:07:11 -0300

    migrations added

---
+++ src/database/migrations/2025_05_08_235829_create_company_machine_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_machine', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('machine_id');
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies');
+            $table->foreign('machine_id')
+                ->references('id')
+                ->on('machines');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_machine');
+    }
+};
---
+++ src/database/migrations/2025_05_08_235844_create_support_tickets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('support_tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('answer_id')
+                ->nullable();
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('support_tickets');
+    }
+};

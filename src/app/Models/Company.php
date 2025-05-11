7fb7cc2e8a509b9b8973dcdd3ae7b414d75dcf6d
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-05-11 14:49:00 -0300

    cashier partially configured

--- src/app/Models/Company.php
+++ src/app/Models/Company.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class Company extends User implements MustVerifyEmail
 {
-    use Notifiable, HasFactory;
+    use Notifiable, HasFactory, Billable;
 
     protected $table = 'companies'; 
 }
